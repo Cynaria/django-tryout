1638a42dbdba207a287c4e5a8ca037d72242c7af
Cynaria <Elizabeth.J.Cruz@gmail.com>
2014-07-21 10:44:17 -0400

    Created a list index view

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from todos.models import List
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello world! You're at the index!")
+	todo_lists = List.objects.all()[:5]
+	template = loader.get_template('todos/index.html')
+	context = RequestContext(request, {'todo_lists': todo_lists})
+	return HttpResponse(template.render(context))
