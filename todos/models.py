4897311b1066e1a4a6a6c2db70e8f40c5a8b40a1
Cynaria <Elizabeth.J.Cruz@gmail.com>
2014-07-21 09:25:22 -0400

    Added List model and Item model. Then also went on to views a bit

--- todos/models.py
+++ todos/models.py
@@ -6,3 +6,14 @@ from django.db import models
 class List(models.Model):
 	title = models.CharField(max_length=140)
 	description = models.TextField()
+	def __str__(self):
+		return self.title
+
+
+class Item(models.Model):
+	name = models.CharField(max_length=140)
+	description = models.TextField()
+	list = models.ForeignKey(List)
+	completed = models.BooleanField(default = False)
+	def __str__(self):
+		return self.name
