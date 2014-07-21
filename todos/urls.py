9354e4b938ce72615e7e5309189fe3cc9c8faebe
Cynaria <Elizabeth.J.Cruz@gmail.com>
2014-07-21 11:40:31 -0400

    added detail page

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 from todos import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<list_id>\d+)/$', views.detail, name='detail'),
 )
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
@@ -10,4 +10,11 @@ def index(request):
 	todo_lists = List.objects.all()[:5]
 	template = loader.get_template('todos/index.html')
 	context = RequestContext(request, {'todo_lists': todo_lists})
+	return HttpResponse(template.render(context))
+
+def detail(request,list_id):
+	list = get_object_or_404(List, pk=list_id)
+	print(list.item_set.all())
+	template = loader.get_template('todos/details.html')
+	context = RequestContext(request, {'list': list})
 	return HttpResponse(template.render(context))
